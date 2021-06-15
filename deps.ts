4f54c5b95978cba70f4779d8fc494426e7b1d6a6
Yusuke Tanaka <yusuktan@maguro.dev>
2021-06-15 20:53:39 +0900

    fix dep versions

--- deps.ts
+++ deps.ts
@@ -1,9 +1,6 @@
-export { config as dotenvConfig } from "https://deno.land/x/dotenv/mod.ts";
-// @deno-types="https://unpkg.com/date-fns@2.15.0/typings.d.ts"
-export {
-  addDays,
-  formatISO,
-  parse,
-  startOfToday,
-  subDays,
-} from "https://deno.land/x/date_fns/index.js";
+export { config as dotenvConfig } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
+export { default as addDays } from "https://deno.land/x/date_fns@v2.22.1/addDays/index.ts";
+export { default as formatISO } from "https://deno.land/x/date_fns@v2.22.1/formatISO/index.js";
+export { default as parse } from "https://deno.land/x/date_fns@v2.22.1/parse/index.js";
+export { default as startOfToday } from "https://deno.land/x/date_fns@v2.22.1/startOfToday/index.ts";
+export { default as subDays } from "https://deno.land/x/date_fns@v2.22.1/subDays/index.ts";
